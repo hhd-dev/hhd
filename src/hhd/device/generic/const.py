d6679c64ec82dcdd778a4165d35666910fb29545
antheas <git@antheas.dev>
2024-02-04 21:53:10 +0100

    tweak loki config

--- src/hhd/device/generic/const.py
+++ src/hhd/device/generic/const.py
@@ -86,7 +86,7 @@ CONFS = {
     "Loki Max": {
         "name": "Loki Max",
         "hrtimer": True,
-        "mappings": gen_gyro_state("x", False, "y", False, "z", False),
+        "mappings": gen_gyro_state("x", False, "z", True, "y", True),
     },
     # Ayaneo
     "AIR Plus": {
