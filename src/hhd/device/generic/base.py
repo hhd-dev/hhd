d53a0a1e3d1f551992b6369b6e48f9b6eacafb46
antheas <git@antheas.dev>
2024-02-05 19:11:15 +0100

    move tt message up

--- src/hhd/device/generic/base.py
+++ src/hhd/device/generic/base.py
@@ -71,11 +71,11 @@ def plugin_run(
             try:
                 with open("/sys/devices/platform/oxp-platform/tt_toggle", "w") as f:
                     f.write("1")
+                logger.info(f"Turbo button takeover enabled")
             except Exception:
                 logger.warn(
                     f"Turbo takeover failed. Ensure you have the latest oxp-sensors driver installed."
                 )
-            logger.info(f"Turbo button takeover enabled")
 
         try:
             logger.info("Launching emulated controller.")
