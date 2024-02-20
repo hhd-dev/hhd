756ce0b5aed7100b304bbf7203cde8eff1c4b992
antheas <git@antheas.dev>
2024-02-20 15:21:02 +0100

    add support for platform profile

--- src/adjustor/drivers/smu/__init__.py
+++ src/adjustor/drivers/smu/__init__.py
@@ -12,17 +12,69 @@ from hhd.plugins.conf import Config
 logger = logging.getLogger(__name__)
 
 
+def get_platform_choices():
+    try:
+        with open("/sys/firmware/acpi/platform_profile_choices", "r") as f:
+            return f.read().strip().split(" ")
+    except Exception:
+        logger.info(
+            f"Could not enumerate platform profile choices. Disabling platform profile."
+        )
+        return None
+
+
+def set_platform_profile(prof: str):
+    try:
+        with open("/sys/firmware/acpi/platform_profile", "w") as f:
+            f.write(prof)
+        return True
+    except Exception as e:
+        logger.error(f"Could not set platform profile with error:\n{e}")
+        return False
+
+
+def get_platform_profile():
+    try:
+        with open("/sys/firmware/acpi/platform_profile", "r") as f:
+            return f.read().replace("\n", "")
+    except Exception as e:
+        logger.error(f"Could not read platform profile with error:\n{e}")
+        return None
+
+
 class SmuQamPlugin(HHDPlugin):
-    def __init__(self) -> None:
+    def __init__(self, platform_profile: bool = True) -> None:
         self.name = f"adjustor_smu_qam"
         self.priority = 6
         self.log = "smuq"
         self.enabled = False
+        self.initialized = False
+
+        self.check_pp = platform_profile
+        self.old_pp = None
+        self.has_pp = False
 
     def settings(self):
         if not self.enabled:
+            self.initialized = False
             return {}
-        return {"tdp": {"qam": load_relative_yaml("qam.yml")}}
+
+        self.initialized = True
+        out = {"tdp": {"qam": load_relative_yaml("qam.yml")}}
+
+        # Limit platform profile choices or remove
+        choices = get_platform_choices()
+        if choices and self.check_pp:
+            options = out["tdp"]["qam"]["children"]["platform_profile"]["options"]
+            for c in list(options):
+                if c not in choices:
+                    del options[c]
+            self.has_pp = True
+        else:
+            del out["tdp"]["qam"]["children"]["platform_profile"]
+            self.has_pp = False
+
+        return out
 
     def open(
         self,
@@ -33,6 +85,19 @@ class SmuQamPlugin(HHDPlugin):
 
     def update(self, conf: Config):
         self.enabled = conf["tdp.general.enable"].to(bool)
+        if not self.enabled or not self.initialized:
+            return
+
+        if self.has_pp:
+            cpp = conf["tdp.qam.platform_profile"].to(str)
+
+            if cpp and cpp != self.old_pp:
+                logger.info(f"Setting platform profile to '{cpp}'")
+                set_platform_profile(cpp)
+
+            pp = get_platform_profile()
+            conf["tdp.qam.platform_profile"] = pp
+            self.old_pp = pp
 
     def close(self):
         pass
