63bedc13a8b82caa79746a5f478733a67383aee5
antheas <git@antheas.dev>
2024-03-01 22:49:36 +0100

    fix deadlock

--- src/adjustor/events.py
+++ src/adjustor/events.py
@@ -1,4 +1,5 @@
 import logging
+import select
 from threading import Event as TEvent
 from typing import Any, Sequence
 
@@ -15,31 +16,37 @@ EVENT_MATCHES: Sequence[tuple[dict[str, Any], str]] = [
     ({"bus_id": b"D320289E-8FEA-"}, "tdp"),
 ]
 
+GUARD_DELAY = 0.5
+
 
 def loop_process_events(emit: Emitter, should_exit: TEvent):
     acpi = AcpiEventSocket()
     logger.info(f"Starting ACPI Event handler.")
 
     while not should_exit.is_set():
-        for message in acpi.get():
-            ev = message.get("ACPI_GENL_ATTR_EVENT", None)
+        # FIXME: Uses unofficial API, find the correct way.
+        r, _, _ = select.select([acpi._sock.fileno()], [], [], GUARD_DELAY)
 
-            if not ev:
-                continue
+        if r:
+            for message in acpi.get():
+                ev = message.get("ACPI_GENL_ATTR_EVENT", None)
 
-            found = False
-            for match, etype in EVENT_MATCHES:
-                matches = True
-                for k, v in match.items():
-                    if k not in ev or ev[k] != v:
-                        matches = False
+                if not ev:
+                    continue
+
+                found = False
+                for match, etype in EVENT_MATCHES:
+                    matches = True
+                    for k, v in match.items():
+                        if k not in ev or ev[k] != v:
+                            matches = False
+                            break
+
+                    if matches:
+                        if etype != "battery":
+                            emit({"type": "acpi", "event": etype})
+                        found = True
                         break
 
-                if matches:
-                    if etype != "battery":
-                        emit({"type": "acpi", "event": etype})
-                    found = True
-                    break
-
-            if not found:
-                logger.info(f"Unknown ACPI event: {ev}")
+                if not found:
+                    logger.info(f"Unknown ACPI event: {ev}")
