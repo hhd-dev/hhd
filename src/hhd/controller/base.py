afa24e1a6c143fc9d180a62585833144451818b9
antheas <git@antheas.dev>
2024-03-04 17:01:19 +0100

    switch to press to avoid closing the overly when reloading the controller

--- src/hhd/controller/base.py
+++ src/hhd/controller/base.py
@@ -464,7 +464,7 @@ class Multiplexer:
                             self.select_is_held = False
                             self.select_pressed = None
 
-                    if self.emit and ev["code"] == "mode" and not ev["value"]:
+                    if self.emit and ev["code"] == "mode" and ev["value"]:
                         # Steam might do weirdness, emit an event to prepare
                         # the overlay
                         self.emit({"type": "special", "event": "guide"})
