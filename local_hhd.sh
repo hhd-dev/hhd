0c760c36391f06a3db9b176eba26a21a5d554e17
antheas <git@antheas.dev>
2024-06-03 20:00:56 +0200

    update local script

--- local_hhd.sh
+++ local_hhd.sh
@@ -4,13 +4,13 @@ sudo systemctl stop hhd@$(whoami)
 sudo systemctl stop hhd_local@$(whoami)
 sudo pkill hhd
 
-rm -rf ~/.local/share/hhd
-mkdir -p ~/.local/share/hhd
-python -m venv --system-site-packages ~/.local/share/hhd/venv
-~/.local/share/hhd/venv/bin/pip install git+https://github.com/hhd-dev/adjustor git+https://github.com/hhd-dev/hhd
+rm -rf ~/.local/share/hhd-tmp
+mkdir -p ~/.local/share/hhd-tmp
+python -m venv --system-site-packages ~/.local/share/hhd-tmp/venv
+~/.local/share/hhd-tmp/venv/bin/pip install git+https://github.com/hhd-dev/adjustor git+https://github.com/hhd-dev/hhd
 
 FINAL_URL='https://api.github.com/repos/hhd-dev/hhd-ui/releases/latest'
-curl -L $(curl -s "${FINAL_URL}" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/.local/share/hhd/hhd-ui
-chmod +x $HOME/.local/share/hhd/hhd-ui
+curl -L $(curl -s "${FINAL_URL}" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/.local/share/hhd-tmp/hhd-ui
+chmod +x $HOME/.local/share/hhd-tmp/hhd-ui
 
-nohup sudo HHD_OVERLAY="$HOME/.local/share/hhd/hhd-ui" ~/.local/share/hhd/venv/bin/hhd --user $(whoami) &> /dev/null &
+nohup sudo HHD_OVERLAY="$HOME/.local/share/hhd-tmp/hhd-ui" ~/.local/share/hhd-tmp/venv/bin/hhd --user $(whoami) &> /dev/null &
