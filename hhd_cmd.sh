84ef277b12167dd57a68de16ab13e382248f8901
antheas <git@antheas.dev>
2024-06-17 00:25:42 +0200

    add new hhd bang

--- hhd_cmd.sh
+++ hhd_cmd.sh
@@ -9,8 +9,4 @@ mkdir -p ~/.local/share/hhd-tmp
 python -m venv --system-site-packages ~/.local/share/hhd-tmp/venv
 ~/.local/share/hhd-tmp/venv/bin/pip install git+https://github.com/hhd-dev/adjustor git+https://github.com/hhd-dev/hhd
 
-FINAL_URL='https://api.github.com/repos/hhd-dev/hhd-ui/releases/latest'
-curl -L $(curl -s "${FINAL_URL}" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/.local/share/hhd-tmp/hhd-ui
-chmod +x $HOME/.local/share/hhd-tmp/hhd-ui
-
-nohup sudo HHD_OVERLAY="$HOME/.local/share/hhd-tmp/hhd-ui" ~/.local/share/hhd-tmp/venv/bin/hhd --user $(whoami) &> /dev/null &
+sudo ~/.local/share/hhd-tmp/venv/bin/hhd --user $(whoami)
