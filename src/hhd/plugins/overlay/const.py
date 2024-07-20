9a504c85bb0a38556f434dfba0ef38cf24b22b36
antheas <git@antheas.dev>
2024-07-20 21:26:59 +0300

    use the correct v3 name

--- src/hhd/plugins/overlay/const.py
+++ src/hhd/plugins/overlay/const.py
@@ -21,7 +21,7 @@ TM = TouchScreenMatch
 
 TOUCH_SCREEN_QUIRKS = {
     TM("83E1", name="Legion GO"): TQ(True, False, False),
-    TM("HPPAC", name="MinisForum V3"): TQ(True, True, False),
+    TM("V3", name="MinisForum V3"): TQ(True, True, False),
 }
 
 
