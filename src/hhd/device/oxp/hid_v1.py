0a5b462f7052f757f38af1482287a97fc72de4af
Antheas Kapenekakis <git@antheas.dev>
2024-10-09 18:29:18 +0200

    switch to send feature for X1

--- src/hhd/device/oxp/hid_v1.py
+++ src/hhd/device/oxp/hid_v1.py
@@ -138,7 +138,7 @@ class OxpHidraw(GenericGamepadHidraw):
         if self.queue_cmd and curr - self.next_send > 0:
             cmd = self.queue_cmd.popleft()
             logger.info(f"OXP C: {cmd.hex()}")
-            self.dev.write(cmd)
+            self.dev.send_feature_report(cmd)
             self.next_send = curr + WRITE_DELAY
 
         # Queue needs to flush before switching to next event
