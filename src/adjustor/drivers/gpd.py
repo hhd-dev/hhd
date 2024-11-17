689683755fe705ae7972d55f229a1511ec530f8f
Antheas Kapenekakis <git@antheas.dev>
2024-11-18 00:34:40 +0100

    disable for now

--- src/adjustor/drivers/gpd.py
+++ src/adjustor/drivers/gpd.py
@@ -15,12 +15,25 @@ def handle_gpd_fix():
 
     # Wait for suspend.target to finish
     # While active it returns 0
-    for _ in range(150):
+    for _ in range(250):
         if os.system("systemctl is-active --quiet suspend.target"):
             break
-        time.sleep(0.1)
+        time.sleep(0.2)
+    
+    # Give it a bit of time, otherwise the EC will keep waking up the device
+    time.sleep(5)
 
-    os.system("systemctl suspend")
+    try:
+        with open("/sys/bus/acpi/devices/LNXTHERM:00/thermal_zone/temp", "r") as f:
+            temp = int(f.read().strip())
+            logger.info(f"Thermal zone temp: {temp}")
+    except Exception as e:
+        logger.error(f"Failed to read thermal zone temp: {e}")
+        temp = 0
+
+    if temp == 105_000:
+        logger.info("Fake temperature detected.")
+        os.system("systemctl suspend")
 
 
 class GpdFixPlugin(HHDPlugin):
@@ -52,5 +65,5 @@ class GpdFixPlugin(HHDPlugin):
                 found = True
                 break
 
-        if found:
-            handle_gpd_fix()
+        # if found:
+        #     handle_gpd_fix()
