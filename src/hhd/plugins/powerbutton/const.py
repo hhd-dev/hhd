d9baebcf590641125becc9066587e072d8bef417
honjow <honjow311@gmail.com>
2025-01-23 12:14:04 +0800

    add gpd winmini 2025

--- src/hhd/plugins/powerbutton/const.py
+++ src/hhd/plugins/powerbutton/const.py
@@ -26,6 +26,7 @@ SUPPORTED_DEVICES: Sequence[PowerButtonConfig] = [
     PBC("ROG Ally X", "ROG Ally X RC72LA"),
     PBC("GPT Win 4", "G1618-04"),
     PBC("GPD Win Mini", "G1617-01"),
+    PBC("GPD Win Mini", "G1617-02"),
     PBC("GPD Win Max 2", "G1619-04"),
     PBC("GPD Win Max 2", "G1619-05"),
     PBC("OrangePi G1621-02/G1621-02", "G1621-02"),
