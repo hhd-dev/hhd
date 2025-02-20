00e3da16b0ba5d7e819f35423aa2e3a01e345ef2
Antheas Kapenekakis <git@antheas.dev>
2025-02-20 18:59:41 +0100

    add claw 7 dmi to controller

--- src/hhd/device/generic/const.py
+++ src/hhd/device/generic/const.py
@@ -181,7 +181,13 @@ CONFS = {
         "type": "claw",
         "display_gyro": False,
     },
-    # MSI Claw 8
+    "Claw 7 AI+ A2VM": {
+        "name": "MSI Claw 7 (2nd gen)",
+        "extra_buttons": "none",
+        "btn_mapping": MSI_CLAW_MAPPINGS,
+        "claw": True,
+        "display_gyro": False,
+    },
     "Claw 8 AI+ A2VM": {
         "name": "MSI Claw 8",
         "extra_buttons": "none",
