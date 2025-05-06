13e3b8a830ca35e0a794707014fbe5a7abc869b2
Scott Tsai <scottt.tw@gmail.com>
2025-05-07 02:50:25 +0800

    install.sh: change file contexes if SELinux is in enforcing mode (#168)
    This sets the binaries in the hhd python virtual env from
    	`system_u:object_r:data_home_t:s0`
    to:
    	`system_u:object_r:bin_t:s0`
    
    Tested on Bazzite 42 and should work on Fedora.

--- install.sh
+++ install.sh
@@ -40,12 +40,25 @@ sudo curl https://raw.githubusercontent.com/hhd-dev/hhd/master/usr/lib/systemd/s
 
 # Add hhd to user path
 mkdir -p ~/.local/bin
-ln -s ~/.local/share/hhd/venv/bin/hhd ~/.local/bin/hhd
-ln -s ~/.local/share/hhd/venv/bin/hhd.contrib ~/.local/bin/hhd.contrib
+ln -sf ~/.local/share/hhd/venv/bin/hhd ~/.local/bin/hhd
+ln -sf ~/.local/share/hhd/venv/bin/hhd.contrib ~/.local/bin/hhd.contrib
 
 FINAL_URL='https://api.github.com/repos/hhd-dev/hhd-ui/releases/latest'
 curl -L $(curl -s "${FINAL_URL}" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/.local/bin/hhd-ui
 chmod +x $HOME/.local/bin/hhd-ui
+
+if [ -f /sys/fs/selinux/enforce ]; then
+  # The presence of this file means SELinux is loaded in the kernel.
+  # A value of 0 means Permissive, 1 means Enforcing.
+  selinux_enforcing=$(cat /sys/fs/selinux/enforce)
+  if [[ "$selinux_enforcing" != "0" ]]; then
+    echo "SELinux is loaded and in enforcing mode: changing hhd file security contextes:"
+    # Fedora Atomic derived distros (e.g. bazzite-gnome) have /home as a symlink to /var/home
+    user_home_dir=$(readlink -f $HOME)
+    sudo semanage fcontext -a -t bin_t $user_home_dir/.local/share/hhd/venv/bin/'.*'
+    sudo restorecon -Rv $user_home_dir//.local/share/hhd/venv/bin/
+  fi
+fi
 
 # Start service and reboot
 sudo systemctl enable --now hhd_local@$(whoami)
