9a21f675ca860a41214c35879edd1427c7ed55b1
Antheas Kapenekakis <git@antheas.dev>
2025-06-15 22:27:25 +0200

    remove unecessary entries for claw

--- src/hhd/device/claw/const.py
+++ src/hhd/device/claw/const.py
@@ -13,21 +13,15 @@ CONFS = {
         # "hrtimer": True, Uses sensor fusion garbage? From intel? Will need custom work
         "extra_buttons": "dual",
         "btn_mapping": MSI_CLAW_MAPPINGS,
-        "type": "claw",
-        "display_gyro": False,
     },
     "MS-1T42": {
         "name": "MSI Claw 7 (2nd gen)",
         "extra_buttons": "dual",
         "btn_mapping": MSI_CLAW_MAPPINGS,
-        "type": "claw",
-        "display_gyro": False,
     },
     "MS-1T52": {
         "name": "MSI Claw 8",
         "extra_buttons": "dual",
         "btn_mapping": MSI_CLAW_MAPPINGS,
-        "type": "claw",
-        "display_gyro": False,
     },
 }
