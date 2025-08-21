a06e51a4548a81a183a40b02296a342f80982b4c
Antheas Kapenekakis <git@antheas.dev>
2025-08-21 21:03:21 +0200

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,7 +8,6 @@ RSYNC="rsync -rv --exclude .git --exclude venv --exclude __pycache__'"
 USER=${2:-bazzite}
 
 # python -m venv --system-site-packages ~/hhd-dev/hhd/venv
-# ~/hhd-dev/hhd/venv/bin/pip install -e ~/hhd-dev/adjustor
 # ~/hhd-dev/hhd/venv/bin/pip install -e ~/hhd-dev/hhd
 # sudo chcon -R -u system_u -r object_r --type=bin_t /var/home/$USER/hhd-dev/hhd/venv/bin
 # sudo systemctl disable --now hhd@$(whoami)
@@ -41,12 +40,10 @@ USER=${2:-bazzite}
 
 # set -e
 $RSYNC . $HOST:hhd-dev/hhd
-$RSYNC ../adjustor/ $HOST:hhd-dev/adjustor
-$RSYNC ../hhd-bazzite/ $HOST:hhd-dev/hhd-bazzite
 
 ssh $HOST /bin/bash << EOF
     sudo systemctl restart hhdl
     # sudo systemctl stop hhdl
 EOF
 
-# ssh -t $HOST "sudo HHD_HORI_STEAM=1 HHD_HIDE_ALL=1 ~/hhd-dev/hhd/venv/bin/hhd --user bazzite"
+# ssh -t $HOST "sudo HHD_HORI_STEAM=1 HHD_HIDE_ALL=1 ~/hhd-dev/hhd/venv/bin/hhd"
