070b13ade04e31f5fa3f8c3e11bf7a26565bb798
Antheas Kapenekakis <git@antheas.dev>
2025-10-12 03:07:51 +0800

    add left touchpad emulation

--- src/hhd/plugins/outputs.py
+++ src/hhd/plugins/outputs.py
@@ -18,6 +18,8 @@ from ..controller.virtual.uinput import (
     TOUCHPAD_BUTTON_MAP,
     TOUCHPAD_CAPABILITIES,
     XBOX_ELITE_BUTTON_MAP,
+    LEFT_TOUCHPAD_BUTTON_MAP,
+    LEFT_TOUCHPAD_AXIS_MAP,
     UInputDevice,
 )
 from .plugin import is_steam_gamepad_running, open_steam_kbd
@@ -43,6 +45,7 @@ def get_outputs(
     touchpad_enable: Literal["disabled", "gamemode", "always"] | None = None,
     rgb_init_times: int | None = None,
     extra_buttons: Literal["none", "dual", "quad"] = "dual",
+    dual_touchpad: bool = False,
 ) -> tuple[Sequence[Producer], Sequence[Consumer], Mapping[str, Any]]:
     producers = []
     consumers = []
@@ -260,6 +263,19 @@ def get_outputs(
         )
         producers.append(d)
         consumers.append(d)
+        if dual_touchpad:
+            d = UInputDevice(
+                name="Handheld Daemon Left Touchpad",
+                phys="phys-hhd-left",
+                capabilities=TOUCHPAD_CAPABILITIES,
+                pid=HHD_PID_TOUCHPAD,
+                btn_map=LEFT_TOUCHPAD_BUTTON_MAP,
+                axis_map=LEFT_TOUCHPAD_AXIS_MAP,
+                output_timestamps=True,
+                ignore_cmds=True,
+            )
+            producers.append(d)
+            consumers.append(d)
         uses_touch = True
 
     return (
