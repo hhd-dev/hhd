ab308c452b0b53553865ff97345f33b39fdb6dd4
DKatzDev <dkatzdev@gmail.com>
2025-12-02 10:58:55 -0500

    corrected power light

--- src/adjustor/drivers/lenovo/__init__.py
+++ src/adjustor/drivers/lenovo/__init__.py
@@ -66,7 +66,7 @@ class LenovoDriverPlugin(HHDPlugin):
             self.fppt_ratio = 41/30
             self.performance_tdp = 20
 
-        if go_model == "gos":
+        if go_model != "go":
             self.power_light_v2 = False
         else:
             bios_version = get_bios_version()
