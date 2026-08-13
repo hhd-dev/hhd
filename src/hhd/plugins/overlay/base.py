4722be7a7ad48b7b5c45ee849d21efe9ba6a1105
Antheas Kapenekakis <git@antheas.dev>
2026-08-14 01:34:43 +0200

    use sigkill to gracefully shutdown the overlay, launch it as a process group, use killmode mixed to prevent systemd from killing the overlay
    prevents coredumps

--- src/hhd/plugins/overlay/base.py
+++ src/hhd/plugins/overlay/base.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import select
+import signal
 import subprocess
 import time
 from threading import Event as TEvent
@@ -120,7 +121,10 @@ def loop_manage_desktop(
         logger.warning(f"The overlay process ended with an exception:\n{e}")
     finally:
         logger.info(f"Stopping overlay process.")
-        proc.kill()
+        try:
+            os.killpg(proc.pid, signal.SIGKILL)
+        except ProcessLookupError:
+            pass
         proc.wait()
         emit.grab(False)
 
@@ -308,7 +312,10 @@ def loop_manage_overlay(
             time.sleep(0.2)
         except Exception as e:
             logger.error(f"Error informing overlay:\n{e}")
-        proc.kill()
+        try:
+            os.killpg(proc.pid, signal.SIGKILL)
+        except ProcessLookupError:
+            pass
         proc.wait()
         emit.grab(False)
         if wake_handler:
