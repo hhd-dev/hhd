eadbd56e5315fa173588e850e0dde1ccfcf8fb89
Antheas Kapenekakis <git@antheas.dev>
2026-09-13 21:27:46 +0200

    always apply the apex quirk
    bios version is inconsistent

--- src/hhd/device/oxp/__init__.py
+++ src/hhd/device/oxp/__init__.py
@@ -184,14 +184,6 @@ def autodetect(existing: Sequence[HHDPlugin]) -> Sequence[HHDPlugin]:
 
     dconf = CONFS.get(dmi, None)
     if dconf:
-        if dmi == "ONEXPLAYER APEX":
-            try:
-                with open("/sys/devices/virtual/dmi/id/bios_version") as f:
-                    bios = tuple(map(int, f.read().strip().split(".")))
-                if bios < (2, 17):
-                    dconf = {**dconf, "quirk": None}
-            except (OSError, ValueError):
-                pass
         return [GenericControllersPlugin(dmi, dconf)]
 
     # Begin hw agnostic dmi match
